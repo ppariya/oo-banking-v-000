27a00b1f24f5508b52313dfe4cf8f9bbb8a86a79
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-23 00:43:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,12 +17,8 @@ class BankAccount
   end
 
   def valid?
-    if @balance > 0
-      return true
-    else
-
-      return false
-    end
+    @balance > 0
+    @status = "open"
   end
 
   def close_account
