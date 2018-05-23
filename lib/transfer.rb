bcccf9a29406864b4c1f5b597983ce49dcefd00c
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-23 01:16:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
         sender.balance -= amount
         receiver.deposit(amount)
         @status = "complete"
-      
+
     else
       @status = "rejected"
       puts "Transaction rejected. Please check your account balance."
